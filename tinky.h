f9f0dc5edcc9a36ca0ac0949531941199a270ad1
Sasha <sasha.lorion@gmail.com>
2024-08-07 20:06:41 +0200

    [ADD] - prototypes & new define

--- tinky.h
+++ tinky.h
@@ -14,17 +14,31 @@
 #include <errno.h>
 #include <tlhelp32.h>
 
-#define SVCNAME TEXT("svc")
+#define SVCNAME TEXT("tinky")
+#define EXECNAME TEXT("winky")
+
+#define CLOSESERVICE(service) CloseServiceHandle(service)
+
 
 extern SERVICE_STATUS			gSvcStatus;
 extern SERVICE_STATUS_HANDLE	gSvcStatusHandle;
 extern HANDLE					ghSvcStopEvent;
 
-VOID	SvcInstall(SC_HANDLE &scm, TCHAR* path);
+// Options
+DWORD	SvcInstall(SC_HANDLE& scm, TCHAR* path);
+DWORD	SvcStart(SC_HANDLE scm, char* path);
+DWORD	SvcStop(SC_HANDLE scm);
 VOID	writeLogsToFile(const std::wstring& logMessage);
-void	createProcess(const char* arg);
 
-VOID WINAPI	svcCtrlHandler(DWORD dwCtrl);
-VOID WINAPI	svcMain(DWORD argc, LPTSTR* argv);
+// Process
+void	createProcess(const char* arg);
+DWORD	findProcessById(const char* svcName);
+void	killProcessById(DWORD pid);
+
+// Service
+VOID	writeLogsToFile(const std::wstring& logMessage);
+VOID	WINAPI svcCtrlHandler(DWORD dwCtrl);
+VOID	WINAPI svcMain(DWORD argc, LPTSTR* argv);
 VOID	svcInit(DWORD argc, LPTSTR* argv);
+VOID	ReportSvcStatus(DWORD currentState, DWORD win32ExitCode, DWORD waitHint);
 #endif
